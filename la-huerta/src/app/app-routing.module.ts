4f48ad9f190b00e1cc9821ec1958240d204d7333
jhurtado96 <javierhurtado98@gmail.com>
2021-04-05 18:56:47 +0200

    headerRegular

--- la-huerta/src/app/app-routing.module.ts
+++ la-huerta/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './pages/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"header",component:HeaderComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
